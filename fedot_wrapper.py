e6fe57c093bff64e2aa2a7aec27398a0938d82b3
maiia <mayaprr@yandex.ru>
2022-09-30 20:46:40 +0300

    refactor

--- fedot_wrapper.py
+++ fedot_wrapper.py
@@ -7,58 +7,67 @@ from fedot.api.main import Fedot
 from fedot.core.data.data import InputData
 from fedot.core.data.data_split import train_test_data_setup
 from fedot.core.pipelines.tuning.unified import PipelineTuner
-from fedot.core.pipelines.tuning
+from fedot.core.pipelines.tuning.tuner_builder import TunerBuilder
+from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.quality_metrics_repository import RegressionMetricsEnum
 from fedot.core.repository.tasks import TaskTypesEnum, Task
 from sklearn.metrics import mean_squared_error as rmse
+from sklearn.model_selection import train_test_split
 
 
 class FedotWrapper:
     def __init__(self, path_to_data_dir: str):
         self.path_to_data_dir = path_to_data_dir
 
-    def run_oil_example(self, is_visualise=True):
+    def run(self, fit_type: str = 'interactive', is_visualise=True):
         problem = 'regression'
-        train_path = Path(self.path_to_data_dir, 'x_train.csv')
-        test_path = Path(self.path_to_data_dir, 'x_test.csv')
-        target_cols = ['Глубина  проникания иглы при 0 °С, [мм-1]',
-                       'Глубина  проникания иглы при 25 °С, [мм-1]',
-                       'Растяжимость  при температуре 0 °С, [см]',
-                       'Температура размягчения, [°С]', 'Эластичность при 0 °С, [%]']
+        train_x_path = Path(self.path_to_data_dir, 'x_train.csv')
+        train_y_path = Path(self.path_to_data_dir, 'y_train.csv')
+        # for true predict
+        # test_x_path = Path(self.path_to_data_dir, 'x_test.csv')
 
-        train_data = InputData.from_csv(train_path,
-                                        target_columns=target_cols,
-                                        task=Task(TaskTypesEnum.regression))
-        train, test = train_test_data_setup(train_data, split_ratio=0.75)
+        x_train = pd.read_csv(train_x_path).drop(['id'], axis=1)
+        y_train = pd.read_csv(train_y_path).drop(['id'], axis=1)
 
-        val_data = InputData.from_csv(test_path,
-                                      target_columns=None,
-                                      task=Task(TaskTypesEnum.regression))
+        y_train = y_train.fillna(np.mean(y_train))
+
+        x_train, x_test, y_train, y_test = train_test_split(x_train, y_train)
+
+        data_train = InputData(task=Task(TaskTypesEnum.regression), data_type=DataTypesEnum.table,
+                               idx=range(len(x_train)),
+                               features=x_train.values,
+                               target=y_train.values)
+
+        data_test = InputData(task=Task(TaskTypesEnum.regression), data_type=DataTypesEnum.table,
+                              idx=range(len(x_test)),
+                              features=x_test.values,
+                              target=y_test.values)
 
         auto_model = Fedot(problem=problem, seed=42, timeout=3, safe_mode=False,
                            metric='rmse', preset='best_quality', n_jobs=6, with_tuning=False)
 
-        auto_model.fit(features=train, target=train.target)
-        comp_prediction = auto_model.predict(features=test)
+        if fit_type == 'normal':
+            auto_model.fit(features=data_train.features, target=data_train.target)
+        elif fit_type == 'iterative':
+            pass
         metrics_comp = auto_model.get_metrics()
         pipeline = auto_model.current_pipeline
         print(f'RMSE after composing {metrics_comp["rmse"]}')
-        tuner = TunerBuilder(train.task) \
+        tuner = TunerBuilder(data_train.task) \
             .with_tuner(PipelineTuner) \
             .with_metric(RegressionMetricsEnum.RMSE) \
             .with_iterations(100) \
-            .build(train)
+            .build(data_train)
 
         tuned_pipeline = tuner.tune(pipeline)
-        tuned_prediction = tuned_pipeline.predict(test).predict
-        metrics_tuned = rmse(test.target, tuned_prediction, squared=False)
+        tuned_prediction = tuned_pipeline.predict(data_test).predict
+        metrics_tuned = rmse(data_test.target, tuned_prediction, squared=False)
         print(f'RMSE after tuning {metrics_tuned}')
         if is_visualise:
             auto_model.current_pipeline.show()
 
-        val_pred = tuned_pipeline.predict(val_data).predict
-        self.save_prediction(val_pred, 'oilcode_prediction.csv')
-        return metrics_tuned
+        test_pred = tuned_pipeline.predict(data_test).predict
+        self.save_prediction(test_pred, 'oilcode_prediction.csv')
 
     @staticmethod
     def save_prediction(prediction, path_to_save):
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from fedot_wrapper import FedotWrapper
 BASE_DATA_PATH = os.path.join(os.getcwd(), 'data')
 
 if __name__ == '__main__':
-    FedotWrapper(path_to_data_dir=BASE_DATA_PATH).run_oil_example()
+    FedotWrapper(path_to_data_dir=BASE_DATA_PATH).run()
