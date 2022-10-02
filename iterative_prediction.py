14fd79c5fba46d95d95e02488a245bbf964b1d35
maiia <mayaprr@yandex.ru>
2022-10-02 11:44:12 +0300

    update iterative

--- iterative_prediction.py
+++ iterative_prediction.py
@@ -1,20 +1,15 @@
-import os
-from pprint import pprint
-from statistics import mean
-
 import pandas as pd
+import numpy as np
+from typing import Tuple
+
+from sklearn.model_selection import train_test_split
+
 from fedot.api.main import Fedot
 from fedot.core.pipelines.node import SecondaryNode, PrimaryNode
 from fedot.core.pipelines.pipeline import Pipeline
-from fedot.core.pipelines.tuning.tuner_builder import TunerBuilder
-from fedot.core.pipelines.tuning.unified import PipelineTuner
 from fedot.core.repository.dataset_types import DataTypesEnum
-from fedot.core.repository.quality_metrics_repository import RegressionMetricsEnum
 from fedot.core.repository.tasks import TaskTypesEnum, Task
-from sklearn.model_selection import train_test_split
-import numpy as np
 from fedot.core.data.data import InputData
-from typing import Tuple
 
 from metric_res import get_metric, _nrmse_metric, MOMENTS, WEIGHTS
 
@@ -46,36 +41,37 @@ def _save_results(res_df: pd.DataFrame):
     res_df.to_csv('oil_pred.csv', index=False)
 
 
+def _get_metric(preds, target):
+    columns = ['Глубина  проникания иглы при 0 °С, [мм-1]',
+               'Глубина  проникания иглы при 25 °С, [мм-1]',
+               'Растяжимость  при температуре 0 °С, [см]',
+               'Температура размягчения, [°С]',
+               'Эластичность при 0 °С, [%]']
+    id_col = pd.DataFrame(np.arange(len(preds)), columns=['id'])
+    preds = id_col.join(pd.DataFrame(preds, columns=columns))
+    target = id_col.join(target)
+    return get_metric(preds, target)
+
+
 def iterative_prediction(x_train: pd.DataFrame, x_test: pd.DataFrame,
                          y_train: pd.DataFrame, y_test: pd.DataFrame, REAL_X_TEST: pd.DataFrame):
-    """ Iteratively predicts targets and adds a target to features to predict the next """
-    results = []
 
-    # init result df with columns names
     res_df = pd.DataFrame(columns=['id',
                                    'Глубина  проникания иглы при 0 °С, [мм-1]',
                                    'Глубина  проникания иглы при 25 °С, [мм-1]',
                                    'Растяжимость  при температуре 0 °С, [см]',
                                    'Температура размягчения, [°С]',
                                    'Эластичность при 0 °С, [%]'])
-
-    for i in reversed(y_test.columns):
+    res_test = []
+    for i in y_test.columns:
         print(i)
         data_train = InputData(task=Task(TaskTypesEnum.regression), data_type=DataTypesEnum.table,
                                idx=range(len(x_train)),
                                features=x_train.values,
                                target=y_train[i].values)
-
         fedot = Fedot(problem='regression', timeout=1, with_tuning=False)
         model = fedot.fit(data_train, predefined_model=_get_pipeline())
 
-        # Tune model
-        tuner = TunerBuilder(data_train.task) \
-            .with_tuner(PipelineTuner) \
-            .with_metric(RegressionMetricsEnum.RMSE) \
-            .with_iterations(50) \
-            .build(data_train)
-        model = tuner.tune(model)
         data_test = InputData(task=Task(TaskTypesEnum.regression), data_type=DataTypesEnum.table,
                               idx=range(len(x_test)),
                               features=x_test.values,
@@ -87,25 +83,28 @@ def iterative_prediction(x_train: pd.DataFrame, x_test: pd.DataFrame,
 
         preds_train = model.predict(data_train).predict
 
-        # concat dataframes
         x_train.reset_index(inplace=True, drop=True)
         x_test.reset_index(inplace=True, drop=True)
         res_df.reset_index(inplace=True, drop=True)
         x_train = pd.concat(
             [x_train, pd.DataFrame(preds_train, columns=[i])], axis=1)
 
+        # collect predictions to calculate metric
         preds_test = model.predict(data_test).predict
-        results.append(_nrmse_metric(preds_test, data_test.target, MOMENTS[i]) * WEIGHTS[i])
+        res_test.append(preds_test)
 
         x_test = pd.concat([x_test, pd.DataFrame(preds_test, columns=[i])], axis=1)
         preds_real_test = model.predict(data_real_test).predict
         REAL_X_TEST = pd.concat([REAL_X_TEST, pd.DataFrame(preds_real_test, columns=[i])], axis=1)
         res_df[i] = np.ravel(preds_real_test)
+    y_pred = np.concatenate(res_test, axis=1)
 
-        _save_results(res_df=res_df)
+    # show metric
+    wnrmse = _get_metric(y_pred, y_test)
+    print(wnrmse)
 
-        # print result metric
-        pprint(mean(results))
+    res_df['id'] = REAL_X_TEST['id']
+    _save_results(res_df=res_df)
 
 
 if __name__ == '__main__':
